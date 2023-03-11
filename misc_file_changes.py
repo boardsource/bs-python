18bd7261eaab5cc2a32f833ec7ee5af1d179a9a0
cole <smithcole@protonmail.com>
2023-03-11 13:54:20 -0800

    drive name

--- misc_file_changes.py
+++ misc_file_changes.py
@@ -13,6 +13,10 @@ def supervisor_file():
     file_path = "./cpy/circuitpython/supervisor/supervisor.mk"
     find_and_replace_in_file(file_path,"CircuitPython","BsPython")
     find_and_replace_in_file(file_path,"circuitpython.org","github.com/boardsource/bs-python")
-    
+def filesystem_file():
+    file_path = "./cpy/circuitpython/supervisor/shared/filesystem.c"
+    find_and_replace_in_file(file_path,'"CIRCUITPY")','"BSPYTHON")')
+    find_and_replace_in_file(file_path,"print(\"Hello World!\")\n","from kmk.json_keymap import JsonMap\nkeyboard = JsonMap(\"layout.json\").return_keyboard()\nif __name__ == \"__main__\":\n    keyboard.go()\n")
 def do_all_the_changes():
     supervisor_file()
+    filesystem_file()
