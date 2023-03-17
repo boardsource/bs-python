389a927b80b39afcf2c7b6a9f30e83920248618d
cole <smithcole@protonmail.com>
2023-03-16 20:31:04 -0700

    real boot.py

--- misc_file_changes.py
+++ misc_file_changes.py
@@ -24,8 +24,7 @@ def filesystem_file():
     file_path = "./cpy/circuitpython/supervisor/shared/filesystem.c"
     find_and_replace_in_file(file_path,'"CIRCUITPY")','"BSPYTHON")')
     find_and_replace_in_file(file_path,'const byte buffer[] = "print(\\"Hello World!\\")\\n";','const byte buffer[] = "from kmk.json_keymap import JsonMap\\nkeyboard = JsonMap(\\"layout.json\\").return_keyboard()\\nif __name__ == \\"__main__\\":\\n    keyboard.go()\\n";')
-    find_and_replace_in_file(file_path,'// Create or modify existing code.py file','const byte buffer2[] = "print(\\"Hello World!\\")\\n";\n    f_open(fatfs, &fs, "/boot.py", FA_WRITE | FA_CREATE_ALWAYS);\n    f_write(&fs, buffer2, sizeof(buffer2) - 1, &char_written);\n    f_close(&fs);\n')
-
+    find_and_replace_in_file(file_path,'// Create or modify existing code.py file','const byte buffer2[] = "from kmk.json_boot import JsonBoot\\nboot = JsonBoot(\\"layout.json\\")\\n";\n    f_open(fatfs, &fs, "/boot.py", FA_WRITE | FA_CREATE_ALWAYS);\n    f_write(&fs, buffer2, sizeof(buffer2) - 1, &char_written);\n    f_close(&fs);\n')
 def do_all_the_changes():
     supervisor_file()
     filesystem_file()
