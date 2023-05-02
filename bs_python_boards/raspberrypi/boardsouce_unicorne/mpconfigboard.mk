fdc12c79389e4ecf98ca598d5860810031ac02bd
Cole Smith <smithcole@protonmail.com>
2023-05-02 09:19:55 -0700

    unicorne

---
+++ bs_python_boards/raspberrypi/boardsouce_unicorne/mpconfigboard.mk
@@ -0,0 +1,14 @@
+USB_VID = 0x4273
+USB_PID = 0x7685
+USB_PRODUCT = "unicorne"
+USB_MANUFACTURER = "Boardsource"
+
+CHIP_VARIANT = RP2040
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+
+
+# CIRCUITPY__EVE = 1
