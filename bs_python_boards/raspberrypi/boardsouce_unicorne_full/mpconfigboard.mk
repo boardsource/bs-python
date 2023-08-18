421954eb4aede62102ca2c00e9a73abf6fa58c73
Cole Smith <smithcole@protonmail.com>
2023-08-18 12:17:49 -0700

    simpli IO

--- bs_python_boards/raspberrypi/boardsouce_unicorne_full/mpconfigboard.mk
+++ bs_python_boards/raspberrypi/boardsouce_unicorne_full/mpconfigboard.mk
@@ -8,7 +8,9 @@ CHIP_FAMILY = rp2
 
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
 
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SimpleIO
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DisplayIO_SSD1306
 FROZEN_MPY_DIRS += $(TOP)/frozen/peg_kmk_firmware/src
