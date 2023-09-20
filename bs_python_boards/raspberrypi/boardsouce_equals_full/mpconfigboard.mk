8ab070893d3f83c8e0a95b1b680bebf8701ae571
Cole Smith <smithcole@protonmail.com>
2023-09-20 09:21:10 -0700

    forgot a lib

--- bs_python_boards/raspberrypi/boardsouce_equals_full/mpconfigboard.mk
+++ bs_python_boards/raspberrypi/boardsouce_equals_full/mpconfigboard.mk
@@ -8,6 +8,7 @@ CHIP_FAMILY = rp2
 
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
 
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SimpleIO
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DisplayIO_SSD1306
